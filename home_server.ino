39e6be5a973cd1ff8e616351a2d71fab4f24f46f
stevstrong <stevestrong@online.de>
2014-12-05 10:45:58 +0100

    Update home_server.ino
    cosmetics

--- home_server.ino
+++ home_server.ino
@@ -70,30 +70,6 @@ void ConnectToHost(void)
     else Serial.println(repl);
   }
 }
-///////////////////////////////////////////////////////////////////////////////
-// send an NTP request to the time server at the given address
-///////////////////////////////////////////////////////////////////////////////
-unsigned long sendNTPpacket(IPAddress& address)
-{
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011; // LI, Version, Mode
-  packetBuffer[1] = 0; // Stratum, or type of clock
-  packetBuffer[2] = 6; // Polling Interval
-  packetBuffer[3] = 0xEC; // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12] = 49;
-  packetBuffer[13] = 0x4E;
-  packetBuffer[14] = 49;
-  packetBuffer[15] = 52;
-  // all NTP fields have been given values, now you can send a packet requesting a timestamp:
-  
-  Udp.beginPacket(address, 123); //NTP requests are to port 123
-  Udp.write(packetBuffer,NTP_PACKET_SIZE);
-  Udp.endPacket();
-}
 ///////////////////////////////////////////////////////////////////
 boolean isLeapYear(unsigned int year) { return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)); }
 ///////////////////////////////////////////////////////////////////
@@ -156,6 +132,30 @@ void PrintDateTime(uint32_t timeStamp)
   if(second < 10) Serial.print("0");
   Serial.println(second);
   // Serial.println();
+}
+///////////////////////////////////////////////////////////////////////////////
+// send an NTP request to the time server at the given address
+///////////////////////////////////////////////////////////////////////////////
+void sendNTPpacket(IPAddress& address)
+{
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011; // LI, Version, Mode
+  packetBuffer[1] = 0; // Stratum, or type of clock
+  packetBuffer[2] = 6; // Polling Interval
+  packetBuffer[3] = 0xEC; // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12] = 49;
+  packetBuffer[13] = 0x4E;
+  packetBuffer[14] = 49;
+  packetBuffer[15] = 52;
+  // all NTP fields have been given values, now you can send a packet requesting a timestamp:
+  
+  Udp.beginPacket(address, 123); //NTP requests are to port 123
+  Udp.write(packetBuffer,NTP_PACKET_SIZE);
+  Udp.endPacket();
 }
 ///////////////////////////////////////////////////////////////////////////
 void GetTime(void)
