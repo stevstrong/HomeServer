7a8bc0e4a4a6fac07cd48e9dbdc556256a2be212
stevstrong <stevestrong@online.de>
2015-06-10 11:53:29 +0200

    Update ether_server.cpp
    - saving also the host address from HTTP header (second header line)
    - cleaning up commented out parts

--- source/ether_server.cpp
+++ source/ether_server.cpp
@@ -43,10 +43,11 @@ void Ether_ServerInit(void)
 /*****************************************************************************/
 void Ether_ServerReceiveData(EthernetClient cli)
 {
-#if _DEBUG_>0
-//      Serial.print(F("ether server received: "));
+#if _DEBUG_>1
+   Serial.print(F("ether server received: "));
 #endif
   char c;
+  uint8_t nl_count = 0;
   s_ind = 0;
     // read and store the first header line only
     while ( cli.available() && s_ind<SERVER_BUFFER_MAX_SIZE )
@@ -56,7 +57,8 @@ void Ether_ServerReceiveData(EthernetClient cli)
 //      if ( s_line>0 )  Serial.write(c);
 #endif
       if ( c=='\r' )  continue;  // take next char
-      if ( c=='\n' )  break;      // end of line
+      if ( c=='\n' )
+      	if ( nl_count++ >0 )	break;  // stop storing at second new line character - we need "Host: bla-bla"
       s_buf[s_ind++] = c;
     }
     // only store limited number of chars, the rest will be ignored.
@@ -73,7 +75,6 @@ void Ether_ServerReceiveData(EthernetClient cli)
 /*****************************************************************************/
 void Ether_ServerCheckForClient(void)
 {
-  //Serial.println(F("checking server..."));
   // listen for incoming clients
   EthernetClient s_client = my_server.available();
   if ( !s_client)  return;
@@ -95,8 +96,7 @@ void Ether_ServerCheckForClient(void)
     // client is same as previous one
     if ( allow )  accessTimeout = millis() + 5*60*1000;  // renew timeout if it is registered IP
   } else {
-    // new client
-    // check if client is already recorded
+    // new client. check if it is already recorded
     byte i=0,found=0;
     for (; i<100; i++) {
       strcpy_P(s_buf, PSTR("clients.txt"));
@@ -105,9 +105,7 @@ void Ether_ServerCheckForClient(void)
       // the line is in s_buf. parse the Ip address
       char * chrPtr = strtok(s_buf, " ");
       chrPtr = strtok(NULL, " .");
-  //    Serial.println(chrPtr);
       if ( remoteip[0]==atoi(strtok(NULL, " .")) && remoteip[1]==atoi(strtok(NULL, ".")) && remoteip[2]==atoi(strtok(NULL, ".")) && remoteip[3]==atoi(strtok(NULL, ".")) ) {
-  //      allow = 1; accessTimeout = millis() + 5*60;  // allow access for 5 minutes
        Serial.print(F("client has index ")); Serial.println(i);
        found = 1;
        break;
@@ -133,7 +131,6 @@ void Ether_ServerCheckForClient(void)
 #endif
   s_client.stop();
 }
-
 /***********************************************************************************/
 void putdecimal(word w, char * s, byte digs)
 {
@@ -169,7 +166,6 @@ void Ether_PrintStandardHeader(EthernetClient cl)
 	cl.println(F("<!DOCTYPE HTML>\n<html>\n<head><meta charset='UTF-8'>\n<title>My homepage</title></head>\n"));
 	cl.println(F("<body>\n<p>&emsp;<a href='/'>Home</a>&emsp;<a href='/files/'>SD-Card</a></p>"));
 	cl.print(F("<p>It is ")); cl.print(dayStr(weekday())); cl.print(", 20"); cl.print(date_str); cl.print(", "); cl.print(time_str); cl.println(F("</p>"));
-	//cl.print("20"); cl.print(date_str); cl.print(", "); cl.print(time_str); cl.println(F("</p>"));
 }
 /***********************************************************************************/
 /***********************************************************************************/
@@ -184,65 +180,8 @@ void Ether_ServerProcessData(EthernetClient cl)
 			allow = 1;
 			accessTimeout = millis() + 5*60*1000;  // allow access for 5 minutes
 			goto bad_request_1;  // tricky 'bad request' response for unknown users
-		}
-      else if ( 0 )   //s_buf[5]==' ' )  // nothing specified
-		{
-			// load index page from SD-card line by line and add parameters value to the end
-			strcpy_P(s_buf,PSTR("web/index.htm"));
-			if ( !file.open(s_buf, O_READ) ) {
-				//File_LogError(PSTR("Could not open 'index.htm'!"), NEW_ENTRY | ADD_NL | P_MEM);
-				Serial.println(F("Could not open 'web/index.htm'!"));
-				goto bad_request_1;
-			}
-			File_SendFile(cl);
-			file.close();
-			// send here the additional data
-			cl.print(F("<div class='myp'>\n<p id='crtDateTime'>"));
-			sprintf(s_buf, "%s - %s - %s", dayStr(weekday()), date_str, time_str);
-			cl.print(s_buf);
-			cl.print(F("</p>\n<p id='params'>"));
-			File_GetRecordLine(0);
-			cl.print(s_buf);
-			cl.print(F("</p>\n<p id='values'>"));
-			File_GetRecordLine(-1);
-			cl.print(s_buf);
-			cl.println(F("</p>\n</div>\n<script>ParseData();</script>"));
-/*
-			// send a standard HTTP response header
-			//Ether_PrintStandardHeader(cl);
-#if 0
-			Vito_ReadParameters();
-			EC_ReadValue();
-			Serial.print(F("read line: ")); Serial.println(param_readings);
-			char * ptr = strtok(param_readings, ",");
-#else
-			File_GetRecordLine(-1);
-			//Serial.print(F("read line: ")); Serial.println(s_buf);
-			char * ptr = strtok(s_buf, ",");
-#endif
-			cl.println(F("<style>th,td{text-align:right;}</style>"));
-			cl.println(F("<table style='width:20%'>\n<tr><th style='text-align:left'>Parameter</th><th>Value</th></tr>"));
-			s_ind = 32;
-			for (byte i=0; ; i++) {
-				signed char index = GetKeyIndex((const char *)pgm_read_word(&read_params[i]));  // get the index of the parameter to log
-				if ( index<0 ) break;
-				cl.print(F("<tr><td style='text-align:left'>"));
-				cl.print( Vito_GetParamName(index) );
-				cl.print( F("</td><td>"));
-				if ( ptr!=0 ) { ptr = strtok(NULL, ","); cl.print( ptr ); }
-				cl.println( F("</td></tr>"));
-			}
-			// add EnergyCam absolute and relative values
-			cl.print(F("<tr><td style='text-align:left'>Strom-H_abs</td><td>"));
-			if ( ptr!=0 ) { ptr = strtok(NULL, ","); cl.print( ptr ); }
-			cl.println( F("</td></tr>"));
-			cl.print(F("<tr><td style='text-align:left'>Strom-H_rel</td><td>"));
-			if ( ptr!=0 ) { ptr = strtok(NULL, ",\n"); cl.print( ptr ); }
-			cl.println( F("</td></tr>\n</table>"));
-			cl.println(F("</body></html>"));
-*/
 		} else {	// parse the requested dir/file
-//dir_file_check:
+			//dir_file_check:
 			char * str = strchr(s_buf+5, ' ');	// find next space pointing before 'HTTP1/x'
 			if ( str==0 ) goto bad_request;
 			*str = 0;  // tricky marking end of dir/file to 0
@@ -271,6 +210,5 @@ bad_request_1:
   }
 proc_end:
   // send here required time info
-//  if ( s_buf[0]>0 ) { cl.print(F("\n<p style='text-align:right'>Page sent in ")); cl.print(millis()-time0); cl.println(F(" ms.</p>")); }
   if ( s_buf[0]!=0 ) { cl.print(F("\nPage sent in ")); cl.print(millis()-time0); cl.println(F(" ms.")); }
 }
